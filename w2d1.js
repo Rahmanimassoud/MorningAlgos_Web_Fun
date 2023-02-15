c8d117542ad134865fdb31f62be7a87d112f3b94
Massoud Rahmani <Rahmanimassoud8@gmail.com>
2023-02-14 23:06:36 -0800

    Week 2 Day 1 Algo practice

---
+++ w2d1.js
@@ -0,0 +1,12 @@
+// Working with Math library
+
+let floor = Math.floor(1.9);
+let ceiling = Math.ceil( 1.1 );
+let random = Math.random();
+    
+// console.log(floor);
+// console.log(ceiling);
+// console.log(random);
+
+
+
